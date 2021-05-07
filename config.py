2ae46927e2e2b5ba08610bc3c7e6cd8112144dea
DylanTao94 <dylan.tao94@gmail.com>
2021-05-07 21:25:07 +1000

    Merge branch 'video_nts_net_flow' into video_nts_net_rgb
    # Conflicts:
    #	datasets/haa500_basketball.py

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ SAVE_FREQ = 1
 resume = ''
 test_model = 'model.ckpt'
 save_dir = './models/'
+RGB_MODEL_PATH = ''
+FLOW_MODEL_PATH = ''
--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import torch.utils.data
 from datetime import datetime
 from torch.optim.lr_scheduler import MultiStepLR
 from config import PROPOSAL_NUM, SAVE_FREQ, LR, WD, save_dir
-from core import model
+from core import model as nts_net
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
@@ -39,29 +39,29 @@ parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
                     help='number of data loading workers (default: 0)')
 parser.add_argument('--epochs', default=250, type=int, metavar='N',
                     help='number of total epochs to run')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=2, type=int,
+parser.add_argument('-b', '--batch_size', default=2, type=int,
                     metavar='N', help='mini-batch size (default: 50)')
-parser.add_argument('--iter-size', default=1, type=int,
+parser.add_argument('--iter_size', default=1, type=int,
                     metavar='I', help='iter size as in Caffe to reduce memory usage (default: 5)')
-parser.add_argument('--new_length', default=1, type=int,
+parser.add_argument('--frame_rate', default=1, type=int,
                     metavar='N', help='length of sampled video frames (default: 1)')
 parser.add_argument('--new_width', default=340, type=int,
                     metavar='N', help='resize width (default: 340)')
 parser.add_argument('--new_height', default=256, type=int,
                     metavar='N', help='resize height (default: 256)')
-parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
+parser.add_argument('--lr', '--learning_rate', default=0.001, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--lr_steps', default=[100, 200], type=float, nargs="+",
                     metavar='LRSteps', help='epochs to decay learning rate by 10')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
+parser.add_argument('--weight_decay', '--wd', default=5e-4, type=float,
                     metavar='W', help='weight decay (default: 5e-4)')
-parser.add_argument('--print-freq', default=100, type=int,
+parser.add_argument('--print_freq', default=100, type=int,
                     metavar='N', help='print frequency (default: 100)')
-parser.add_argument('--save-freq', default=1, type=int,
+parser.add_argument('--save_freq', default=1, type=int,
                     metavar='N', help='save frequency (default: 1)')
 parser.add_argument('--resume', default='./checkpoints', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
@@ -70,37 +70,6 @@ parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
 
 global args
 args = parser.parse_args()
-
-# Data transforming
-if args.modality == "rgb":
-    is_color = True
-    scale_ratios = [1.0, 0.875, 0.75, 0.66]
-    clip_mean = [0.485, 0.456, 0.406] * args.new_length
-    clip_std = [0.229, 0.224, 0.225] * args.new_length
-elif args.modality == "flow":
-    is_color = False
-    scale_ratios = [1.0, 0.875, 0.75]
-    clip_mean = [0.5, 0.5] * args.new_length
-    clip_std = [0.226, 0.226] * args.new_length
-else:
-    print("No such modality. Only rgb and flow supported.")
-
-normalize = video_transforms.Normalize(mean=clip_mean,
-                                       std=clip_std)
-train_transform = video_transforms.Compose([
-        # video_transforms.Scale((256)),
-        video_transforms.MultiScaleCrop((224, 224), scale_ratios),
-        video_transforms.RandomHorizontalFlip(),
-        video_transforms.ToTensor(),
-        normalize,
-    ])
-
-val_transform = video_transforms.Compose([
-        # video_transforms.Scale((256)),
-        video_transforms.CenterCrop((224)),
-        video_transforms.ToTensor(),
-        normalize,
-    ])
 
 # data loading
 train_setting_file = "train_%s_split%d.txt" % (args.modality, args.split)
@@ -114,20 +83,18 @@ train_dataset = datasets.__dict__[args.dataset](root=args.data,
                                                 source=train_split_file,
                                                 phase="train",
                                                 modality=args.modality,
-                                                is_color=is_color,
-                                                new_length=args.new_length,
+                                                is_color= args.modality == 'rgb',
+                                                frame_rate=args.frame_rate,
                                                 new_width=args.new_width,
-                                                new_height=args.new_height,
-                                                video_transform=train_transform)
+                                                new_height=args.new_height)
 val_dataset = datasets.__dict__[args.dataset](root=args.data,
                                               source=val_split_file,
                                               phase="val",
                                               modality=args.modality,
-                                              is_color=is_color,
-                                              new_length=args.new_length,
+                                              is_color= args.modality == 'flow',
+                                              frame_rate=args.frame_rate,
                                               new_width=args.new_width,
-                                              new_height=args.new_height,
-                                              video_transform=val_transform)
+                                              new_height=args.new_height)
 
 print('{} samples found, {} train samples and {} test samples.'.format(len(val_dataset)+len(train_dataset),
                                                                        len(train_dataset),
@@ -145,7 +112,7 @@ val_loader = torch.utils.data.DataLoader(
 
 # create model
 print("Building model ... ")
-net = model.attention_net(topN=PROPOSAL_NUM)
+model = nts_net.attention_net(topN=PROPOSAL_NUM)
 
 # define loss function (criterion)
 criterion = torch.nn.CrossEntropyLoss().to(device)
@@ -155,10 +122,10 @@ if not os.path.exists(args.resume):
 print("Saving everything to directory %s." % (args.resume))
 
 # define optimizers
-raw_parameters = list(net.pretrained_model.parameters())
-part_parameters = list(net.proposal_net.parameters())
-concat_parameters = list(net.concat_net.parameters())
-partcls_parameters = list(net.partcls_net.parameters())
+raw_parameters = list(model.pretrained_model.parameters())
+part_parameters = list(model.proposal_net.parameters())
+concat_parameters = list(model.concat_net.parameters())
+partcls_parameters = list(model.partcls_net.parameters())
 
 raw_optimizer = torch.optim.SGD(raw_parameters, lr=LR, momentum=0.9, weight_decay=WD)
 concat_optimizer = torch.optim.SGD(concat_parameters, lr=LR, momentum=0.9, weight_decay=WD)
@@ -168,32 +135,34 @@ schedulers = [MultiStepLR(raw_optimizer, milestones=[60, 100], gamma=0.1),
               MultiStepLR(concat_optimizer, milestones=[60, 100], gamma=0.1),
               MultiStepLR(part_optimizer, milestones=[60, 100], gamma=0.1),
               MultiStepLR(partcls_optimizer, milestones=[60, 100], gamma=0.1)]
-net = net.to(device)
+
+model = model.to(device)
 
 for epoch in range(start_epoch, args.epochs):
     # begin training
-    net.train()
+    model.train()
     for i, (input, target) in enumerate(train_loader):
-        img = input.float().to(device)
-        label = target.to(device)
-        # img, label = data[0].to(device), data[1].to(device)
-        batch_size = img.size(0)
-        raw_logits, concat_logits, part_logits, _, top_n_prob = net(img)
-        #raw_logits: 
-        #concat_logits
-        #part_logits
-        #top_n_prob
+        batch_size = len(target)
+        if args.modality == "rgb":
+            frame = input[0].float().to(device)
+            label = target.to(device)
+            raw_logits, concat_logits, part_logits, _, top_n_prob = model(frame)
+        elif args.modality == "flow":
+            flow_x = input[0].float().to(device)
+            flow_y = input[1].float().to(device)
+            label = target.to(device)
+            raw_logits, concat_logits, part_logits, _, top_n_prob = model(flow_x) + model(flow_y)
 
-        #Teacher Loss 来帮助计算 Navigator Loss 的零时shape
+        # Teacher Loss 来帮助计算 Navigator Loss 的临时shape
         # 与partcls_loss仅仅改了一个shape
-        part_loss = model.list_loss(part_logits.view(batch_size * PROPOSAL_NUM, -1),
+        part_loss = nts_net.list_loss(part_logits.view(batch_size * PROPOSAL_NUM, -1),
                                     label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1)).view(batch_size, PROPOSAL_NUM)
         # Feature detector loss
         raw_loss = criterion(raw_logits, label)
         # Scrutinizing loss
         concat_loss = criterion(concat_logits, label)
         # Navigator loss
-        rank_loss = model.ranking_loss(top_n_prob, part_loss)
+        rank_loss = nts_net.ranking_loss(top_n_prob, part_loss)
         # Teacher loss
         partcls_loss = criterion(part_logits.view(batch_size * PROPOSAL_NUM, -1),
                                  label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1))
@@ -215,133 +184,65 @@ for epoch in range(start_epoch, args.epochs):
         # evaluate in the middle
         if i % args.print_freq == 0:
             # eval train test
-            train_loss = 0
-            train_correct = 0
-            total = 0
-            net.eval()
-            for i, (input, target) in enumerate(train_loader):
-                with torch.no_grad():
-                    img = input.float().to(device)
-                    label = target.to(device)
-                    # img, label = data[0].to(device), data[1].to(device)
-                    batch_size = img.size(0)
-                    _, concat_logits, _, _, _ = net(img)
-                    # calculate loss
-                    concat_loss = criterion(concat_logits, label)
-                    # calculate accuracy
-                    _, concat_predict = torch.max(concat_logits, 1)
-                    total += batch_size
-                    train_correct += torch.sum(concat_predict.data == label.data)
-                    train_loss += concat_loss.item() * batch_size
-
-            train_acc = float(train_correct) / total
-            train_loss = train_loss / total
-
-            print(
-                'epoch:{} - train loss: {:.3f} and train acc: {:.3f} total sample: {}'.format(
-                    epoch,
-                    train_loss,
-                    train_acc,
-                    total))
-
+            train_loss, train_accuracy = eval(model, train_loader, args.modality, "train")
             # eval val test
-            test_loss = 0
-            test_correct = 0
-            total = 0
-            for i, (input, target) in enumerate(val_loader):
-                with torch.no_grad():
-                    img = input.float().to(device)
-                    label = target.to(device)
-                    # img, label = data[0].to(device), data[1].to(device)
-                    batch_size = img.size(0)
-                    _, concat_logits, _, _, _ = net(img)
-                    # calculate loss
-                    concat_loss = criterion(concat_logits, label)
-                    # calculate accuracy
-                    _, concat_predict = torch.max(concat_logits, 1)
-                    total += batch_size
-                    test_correct += torch.sum(concat_predict.data == label.data)
-                    test_loss += concat_loss.item() * batch_size
+            val_loss, val_accuracy = eval(model, val_loader, args.modality, "val")
 
-            test_acc = float(test_correct) / total
-            test_loss = test_loss / total
-            print(
-                'epoch:{} - test loss: {:.3f} and test acc: {:.3f} total sample: {}'.format(
-                    epoch,
-                    test_loss,
-                    test_acc,
-                    total))
-
-    # save model and print trainset evaluation
+    # save model
     if epoch % args.save_freq == 0:
-        train_loss = 0
-        train_correct = 0
-        total = 0
-        net.eval()
-        for i, (input, target) in enumerate(train_loader):
-            with torch.no_grad():
-                img = input.float().to(device)
-                label = target.to(device)
-                # img, label = data[0].to(device), data[1].to(device)
-                batch_size = img.size(0)
-                _, concat_logits, _, _, _ = net(img)
-                # calculate loss
-                concat_loss = criterion(concat_logits, label)
-                # calculate accuracy
-                _, concat_predict = torch.max(concat_logits, 1)
-                total += batch_size
-                train_correct += torch.sum(concat_predict.data == label.data)
-                train_loss += concat_loss.item() * batch_size
-
-        train_acc = float(train_correct) / total
-        train_loss = train_loss / total
-
-        print(
-            'epoch:{} - train loss: {:.3f} and train acc: {:.3f} total sample: {}'.format(
-                epoch,
-                train_loss,
-                train_acc,
-                total))
-
-	# evaluate on test set
-        test_loss = 0
-        test_correct = 0
-        total = 0
-        for i, (input, target) in enumerate(val_loader):
-            with torch.no_grad():
-                img = input.float().to(device)
-                label = target.to(device)
-                # img, label = data[0].to(device), data[1].to(device)
-                batch_size = img.size(0)
-                _, concat_logits, _, _, _ = net(img)
-                # calculate loss
-                concat_loss = criterion(concat_logits, label)
-                # calculate accuracy
-                _, concat_predict = torch.max(concat_logits, 1)
-                total += batch_size
-                test_correct += torch.sum(concat_predict.data == label.data)
-                test_loss += concat_loss.item() * batch_size
-
-        test_acc = float(test_correct) / total
-        test_loss = test_loss / total
-        print(
-            'epoch:{} - test loss: {:.3f} and test acc: {:.3f} total sample: {}'.format(
-                epoch,
-                test_loss,
-                test_acc,
-                total))
-
-	# save model
-        net_state_dict = net.state_dict()
+	    # save model
+        model_state_dict = model.state_dict()
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
         torch.save({
             'epoch': epoch,
             'train_loss': train_loss,
-            'train_acc': train_acc,
-            'test_loss': test_loss,
-            'test_acc': test_acc,
-            'net_state_dict': net_state_dict},
+            'train_acc': train_accuracy,
+            'val_loss': val_loss,
+            'val_acc': val_accuracy,
+            'model_state_dict': model_state_dict},
             os.path.join(save_dir, '%03d.ckpt' % epoch))
 
 print('finishing training')
+
+def eval(model, dataloader, modality, phase):
+    model.eval()
+    loss = 0
+    correct = 0
+    n_sample = 0
+    for i, (input, target) in enumerate(dataloader):
+        with torch.no_grad():
+            if modality == "rgb":
+                frame = input[0].float().to(device)
+                label = target.to(device)
+                raw_logits, concat_logits, part_logits, _, top_n_prob = model(frame)
+            elif modality == "flow":
+                flow_x = input[0].float().to(device)
+                flow_y = input[1].float().to(device)
+                label = target.to(device)
+                raw_logits, concat_logits, part_logits, _, top_n_prob = model(flow_x) + model(flow_y)
+            elif modality == "both":
+                frame = input[0].float().to(device)
+                flow_x = input[1].float().to(device)
+                flow_y = input[2].float().to(device)
+                label = target.to(device)
+                raw_logits, concat_logits, part_logits, _, top_n_prob = model(frame) + model(flow_x) + model(flow_y)
+            # calculate loss
+            concat_loss = criterion(concat_logits, label)
+            # calculate accuracy
+            _, concat_predict = torch.max(concat_logits, 1)
+            n_sample += batch_size
+            correct += torch.sum(concat_predict.data == label.data)
+            loss += concat_loss.item() * batch_size
+
+    accuracy = float(correct) / n_sample
+    loss = loss / n_sample
+    print(
+        'epoch:{} - {} loss: {:.3f} and {} acc: {:.3f} total samples: {}'.format(
+            epoch,
+            phase,
+            loss,
+            phase,
+            accuracy,
+            n_sample))
+    return loss, accuracy
